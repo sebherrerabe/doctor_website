7641cebcf8bec18c807d92aaba465cfd5731cf6f
Sebastian Herrera <sebastian@tapioview.com>
2023-01-12 19:13:09 +0100

    news index finished

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SiteSettingsView, PageListView, NewsListView
+from .views import SiteSettingsView, PageListView, NewsListView, NewsByCategoryListView, NewsByDateListView
 
 
 urlpatterns = [
     path("site-settings/", SiteSettingsView.as_view(), name="site-settings"),
     path("pages/", PageListView.as_view(), name="pages"),
-    path("news/", NewsListView.as_view(), name="news")
+    path("news/", NewsListView.as_view(), name="news"),
+    path("news/category/", NewsByCategoryListView.as_view(), name="news-by-category"),
+    path("news/date/", NewsByDateListView.as_view(), name="news-by-date"),
 ]
